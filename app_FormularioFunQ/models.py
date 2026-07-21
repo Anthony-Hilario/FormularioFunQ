a2a335dbd02b6b673dd9246f7e3a7d414be407a2
Anthony <anthonyhilario.dev@gmail.com>
2026-07-21 16:47:42 -0300

    add alunos no db

--- app_FormularioFunQ/models.py
+++ app_FormularioFunQ/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=255)
+    idade = models.PositiveIntegerField()
+
+
+class RespostaFormulario(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+
+    q1 = models.PositiveSmallIntegerField()
+    q2 = models.PositiveSmallIntegerField()
+    q3 = models.PositiveSmallIntegerField()
+    q4 = models.PositiveSmallIntegerField()
+    q5 = models.PositiveSmallIntegerField()
+    q6 = models.PositiveSmallIntegerField()
+    q7 = models.PositiveSmallIntegerField()
+    q8 = models.PositiveSmallIntegerField()
+    q9 = models.PositiveSmallIntegerField()
+    q10 = models.PositiveSmallIntegerField()
+    q11 = models.PositiveSmallIntegerField()
+    q12 = models.PositiveSmallIntegerField()
+    q13 = models.PositiveSmallIntegerField()
+    q14 = models.PositiveSmallIntegerField()
+    q15 = models.PositiveSmallIntegerField()
+    q16 = models.PositiveSmallIntegerField()
+    q17 = models.PositiveSmallIntegerField()
+    q18 = models.PositiveSmallIntegerField()
+
--- app_FormularioFunQ/views.py
+++ app_FormularioFunQ/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Aluno, RespostaFormulario
 
 # Create your views here.
 def PaginaFormulario(request):
@@ -6,3 +7,27 @@ def PaginaFormulario(request):
 
 def PaginaLogin(request):
     return render(request, 'PagLogin/login.html')
+
+def PaginaAgradecimentos(request):
+    return render(request, 'PagAgradecimentos/agradecimentos.html')
+
+
+
+def Alunos(request):
+    #salvar os dados do aluno para o banco de dados
+    novo_aluno = Aluno()
+    novo_aluno.nome = request.POST.get('name')
+    novo_aluno.idade = request.POST.get('age')
+    novo_aluno.save()
+
+    return redirect('PaginaFormulario')
+
+def Respostas(request):
+    nova_resposta = RespostaFormulario()
+
+    for i in range(1, 19):
+        setattr(nova_resposta, f'q{i}', request.POST.get(f'q{i}'))
+
+    nova_resposta.save()
+
+    return redirect('PaginaAgradecimentos')
